cbf7c3f7b3491d1f207fcde9858b70ef1047f9fc
piriram <pyoram25@gmail.com>
2025-10-11 01:52:53 +0900

    initial commit

--- PillingApp/SceneDelegate.swift
+++ PillingApp/SceneDelegate.swift
@@ -13,10 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        let rootVC = ViewController()
+        // If you want a navigation bar, uncomment the next line and set rootViewController to nav
+        // let nav = UINavigationController(rootViewController: rootVC)
+        window.rootViewController = rootVC
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
