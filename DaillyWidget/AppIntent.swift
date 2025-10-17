74120830f0fa84906b69e02772cb71a4168c1e9d
piriram <pyoram25@gmail.com>
2025-10-17 18:30:09 +0900

    Chore: 위젯 타겟 추가

---
+++ DaillyWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  DaillyWidget
+//
+//  Created by 잠만보김쥬디 on 10/17/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
